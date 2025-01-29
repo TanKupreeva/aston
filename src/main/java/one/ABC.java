b4e38c0db69c10cc93fdc0bc0b50eaff35685ec4
Tatyana Kupreeva <>
2025-01-29 19:21:54 +0300

    how are you

--- src/main/java/one/ABC.java
+++ src/main/java/one/ABC.java
@@ -3,5 +3,6 @@ package one;
 public class ABC {
     public static void main(String[] args) {
         System.out.printf("Hello! Tanya!");
+        System.out.printf("How are you today?");
     }
 }
