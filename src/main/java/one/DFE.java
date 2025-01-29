aada332e3f9f53950ae52865e44305e618f0a1b6
Tatyana Kupreeva <>
2025-01-29 23:31:47 +0300

    cat

--- src/main/java/one/DFE.java
+++ src/main/java/one/DFE.java
@@ -3,6 +3,6 @@ package one;
 public class DFE {
     public static void main(String[] args) {
         System.out.println("Tom");
-        System.out.println("hello tom");
+        System.out.println("hello cat tom");
     }
 }
