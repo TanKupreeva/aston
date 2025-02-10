6f80d5e8f722bb4fe9b41e83cb9dc02d4a4879da
Andrew Hlushakou <andrey.hlushakou@gmail.com>
2025-02-10 22:22:59 +0300

    class selection moved to separate class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,47 +1,9 @@
 package org.example;
 
-import org.example.entity.Book;
-import org.example.entity.Car;
-import org.example.entity.RootVegetable;
-import org.example.menu.*;
-import org.example.menu.selectaction.SelectAction;
-import org.example.menu.selectaction.SelectActionBook;
-import org.example.menu.selectaction.SelectActionCar;
-import org.example.menu.selectaction.SelectActionRootVegetable;
+import org.example.menu.SelectClass;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
-public class App extends CommandImpl
+public class App
 {
-    private final static List<Book> books = new ArrayList<>();
-    private final static List<Car> cars = new ArrayList<>();
-    private final static List<RootVegetable> rootVegetables = new ArrayList<>();
-
-    public App(BufferedReader bufferedReader) {
-        super(bufferedReader);
-        builderMenu.append("Please, select a collection class to operate on:\n");
-        SelectAction<Book> bookSelectAction = new SelectActionBook(bufferedReader, books);
-        SelectAction<Car> carSelectAction = new SelectActionCar(bufferedReader, cars);
-        SelectAction<RootVegetable> rootVegetableSelectAction = new SelectActionRootVegetable(bufferedReader, rootVegetables);
-
-        mapCommands.put(1, bookSelectAction);
-        builderMenu.append(1).append(" - Book.\n");
-
-        mapCommands.put(2, carSelectAction);
-        builderMenu.append(2).append(" - Car.\n");
-
-        mapCommands.put(3, rootVegetableSelectAction);
-        builderMenu.append(3).append(" - RootVegetable.\n");
-
-        mapCommands.put(4, () -> {
-            System.out.println("\nExit the program. Bye bye...");
-            isExecute = false;});
-        builderMenu.append(4).append(" - Exit the program.\n");
-    }
 
     public static void main( String[] args ) {
         System.out.print("***************************************************************************************\n" +
@@ -49,14 +11,9 @@ public class App extends CommandImpl
                 "This is an application that will implement the sorting algorithm for the passed classes\n" +
                 "***************************************************************************************\n");
 
-        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));) {
-            CommandImpl mainCommand = new App(bufferedReader);
-            mainCommand.execute();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("***************************************************************************************");
+        SelectClass.start();
 
+        System.out.println("***************************************************************************************");
     }
 
 }
---
+++ src/main/java/org/example/menu/SelectClass.java
@@ -0,0 +1,53 @@
+package org.example.menu;
+
+import org.example.entity.Book;
+import org.example.entity.Car;
+import org.example.entity.RootVegetable;
+import org.example.menu.selectaction.SelectAction;
+import org.example.menu.selectaction.SelectActionBook;
+import org.example.menu.selectaction.SelectActionCar;
+import org.example.menu.selectaction.SelectActionRootVegetable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectClass extends CommandImpl {
+    private final static List<Book> books = new ArrayList<>();
+    private final static List<Car> cars = new ArrayList<>();
+    private final static List<RootVegetable> rootVegetables = new ArrayList<>();
+
+    private SelectClass(BufferedReader bufferedReader) {
+        super(bufferedReader);
+        builderMenu.append("Please, select a collection class to operate on:\n");
+        SelectAction<Book> bookSelectAction = new SelectActionBook(bufferedReader, books);
+        SelectAction<Car> carSelectAction = new SelectActionCar(bufferedReader, cars);
+        SelectAction<RootVegetable> rootVegetableSelectAction = new SelectActionRootVegetable(bufferedReader, rootVegetables);
+
+        mapCommands.put(1, bookSelectAction);
+        builderMenu.append(1).append(" - Book.\n");
+
+        mapCommands.put(2, carSelectAction);
+        builderMenu.append(2).append(" - Car.\n");
+
+        mapCommands.put(3, rootVegetableSelectAction);
+        builderMenu.append(3).append(" - RootVegetable.\n");
+
+        mapCommands.put(4, () -> {
+            System.out.println("\nExit the program. Bye bye...");
+            isExecute = false;});
+        builderMenu.append(4).append(" - Exit the program.\n");
+    }
+
+    public static void start(){
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));) {
+            CommandImpl mainCommand = new SelectClass(bufferedReader);
+            mainCommand.execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
