7d437ee97675bfb9dd4a66c79deb6a7d023ac67d
Yuliya Chernook <ychernook@devsgroup.net>
2025-02-11 19:29:47 +0300

    move BinarySearch to utils
    (cherry picked from commit b8e34724d335762c02cdb7676c23786ec6deffbc)

--- src/main/java/org/example/utils/BinarySearch.java
+++ src/main/java/org/example/utils/BinarySearch.java
@@ -3,27 +3,29 @@ package org.example.utils;
 import java.util.Comparator;
 import java.util.List;
 
-public final class BinarySearch<T> {
+public final class BinarySearch {
 
     private BinarySearch() {}
 
     public static <T> int search(List<T> list, T key, Comparator<T> comparator) {
         int left = 0;
-        int right = list.size()-1;
+        int right = list.size() - 1;
 
-        while (left <= right ) {
-            int mid = (left + right ) >>> 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             T midVal = list.get(mid);
-            int cmp = comparator.compare(midVal, key);
 
-            if (cmp < 0)
-                left = mid + 1;
-            else if (cmp > 0)
-                right  = mid - 1;
-            else
+            if (comparator.compare(midVal, key) == 0) {
                 return mid; // key found
-        }
-        return -1;  // key not found
-    }
+            }
 
+            if (comparator.compare(midVal, key) < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1; // key not found
+    }
 }
