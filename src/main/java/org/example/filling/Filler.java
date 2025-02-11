79baa9411941dbf6ab8c01648a3487edac43a545
Tatyana Kupreeva <>
2025-02-11 19:01:18 +0300

    abs class Filler

---
+++ src/main/java/org/example/filling/Filler.java
@@ -0,0 +1,61 @@
+package org.example.filling;
+
+
+import java.io.BufferedReader;
+import java.util.List;
+import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
+import java.io.IOException;
+
+public abstract class Filler<T>{
+
+        protected IntConsumer intConsumer;
+
+        protected final BufferedReader bufferedReader;
+
+        protected final List<T> originalList;
+
+        protected final StringBuilder menuBuilder2 = new StringBuilder()
+                .append("\nEnter the number of objects.\n")
+                .append("Or enter \"exit\" for exit.\n");
+
+        public Filler(BufferedReader bufferedReader, List<T> originalList, IntConsumer intConsumer) {
+            this.originalList = originalList;
+            this.bufferedReader = bufferedReader;
+            this.intConsumer = intConsumer;
+        }
+
+        public void enterCapacity() {
+            boolean isWork = true;
+            while (isWork) {
+                System.out.println(menuBuilder2);
+                try {
+                    String line = bufferedReader.readLine();
+                    if (line.equalsIgnoreCase("exit")) {
+                        System.out.print("***************************************************************************************\n" +
+                                "Exit filler.\n" +
+                                "***************************************************************************************\n");
+                        isWork = false;
+                    } else {
+                        isWork = checkInputData(line);
+                    }
+
+                } catch (NumberFormatException ex) {
+                    System.out.print("ERROR. This handler only accepts numbers. Try again.\n");
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+        protected abstract boolean checkInputData(String line);
+
+        protected void fill(int capacity) {
+            IntStream.range(0, capacity).forEach(intConsumer);
+            System.out.print("***************************************************************************************\n" +
+                    "Add " + capacity + " objects.\n" +
+                    "***************************************************************************************\n");
+        }
+    }
