1a2dbfc3da88481a0be13bc4c2b10e835900125f
Yuliya Chernook <ychernook@devsgroup.net>
2025-02-11 19:29:10 +0300

    use list instead of array in BinarySearch

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -1,24 +1,31 @@
 package org.example;
 
 import java.util.Comparator;
+import java.util.List;
 
-public class BinarySearch<T> {
+public final class BinarySearch {
 
-    public static <T> int search(T[] array, T key, Comparator<T> comparator) {
+    private BinarySearch() {}
+
+    public static <T> int search(List<T> list, T key, Comparator<T> comparator) {
         int left = 0;
-        int right = array.length - 1;
+        int right = list.size() - 1;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (array[mid].equals(key)) {
-                return mid;
+            T midVal = list.get(mid);
+
+            if (midVal.equals(key)) {
+                return mid; // key found
             }
-            if (comparator.compare(array[mid], key) < 0) {
+
+            if (comparator.compare(midVal, key) < 0) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        return -1;
+
+        return -1; // key not found
     }
 }
